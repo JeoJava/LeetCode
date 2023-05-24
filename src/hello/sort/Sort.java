3ad6f3d9b48c67266180ce9a09f74eb4e2892688
JeoJava <JeoJava@users.noreply.github.com>
2023-05-24 19:56:15 +0800

    test

---
+++ src/hello/sort/Sort.java
@@ -0,0 +1,13 @@
+package hello.sort;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: xutao
+ * @Date: 2023/05/24 19:52
+ * @Version 1.0
+ * @Description:
+ */
+public interface Sort {
+    public void sort(int[] arr);
+}
