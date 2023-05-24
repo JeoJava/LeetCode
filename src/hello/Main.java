cb1d9076e62ce63af1058c38d7f892c107f6a745
JeoJava <JeoJava@users.noreply.github.com>
2023-05-24 19:51:22 +0800

    Initial commit

---
+++ src/hello/Main.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: xutao
+ * @Date: 2023/05/24 18:09
+ * @Version 1.0
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(111);
+    }
+}
