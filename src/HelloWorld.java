fd17eb1ed3a2e7fc957bb6f5ba3374320979badf
jeojava <2531053148@qq.com>
2024-10-24 17:43:26 +0800

    add hello

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author: XT
+ * @create: 2024-10-24 17:39
+ * @description:
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
